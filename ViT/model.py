b44b3e4ee82befc37dee65969a06c55c18b89a84
Harsh <harshpatel1632@gmail.com>
2025-05-02 17:50:51 -0400

    Clean up testing

--- ViT/model.py
+++ ViT/model.py
@@ -15,24 +15,3 @@ class LayerNormalization(nn.Module):
         z_val = (x - mean) / torch.sqrt(var + self.epsilon)
         return z_val * self.alpha + self.beta
     
-
-def test_layer_norm():
-    x = torch.randn(10, 20)  # batch of 10 samples, each with 20 features
-
-    # Custom LayerNorm
-    my_ln = LayerNormalization()
-    my_ln.alpha.data = torch.ones(1)
-    my_ln.beta.data = torch.zeros(1)
-
-    # PyTorch's LayerNorm
-    torch_ln = nn.LayerNorm(20, eps=1e-6)
-    torch_ln.weight.data = torch.ones(20)
-    torch_ln.bias.data = torch.zeros(20)
-
-    # Run & compare both
-    out_my = my_ln(x)
-    out_torch = torch_ln(x)
-    print("Mean absolute difference:", torch.mean(torch.abs(out_my - out_torch)))
-
-test_layer_norm()
-
---
+++ ViT/test.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+from model import LayerNormalization
+
+def test_layer_norm():
+    x = torch.randn(10, 20)  # batch of 10 samples, each with 20 features
+
+    # Custom LayerNorm
+    my_ln = LayerNormalization()
+    my_ln.alpha.data = torch.ones(1)
+    my_ln.beta.data = torch.zeros(1)
+
+    # PyTorch's LayerNorm
+    torch_ln = nn.LayerNorm(20, eps=1e-6)
+    torch_ln.weight.data = torch.ones(20)
+    torch_ln.bias.data = torch.zeros(20)
+
+    # Run & compare both
+    out_my = my_ln(x)
+    out_torch = torch_ln(x)
+
+    diff = torch.mean(torch.abs(out_my - out_torch))
+    threshold = 1e-4
+    
+    print("Mean absolute difference:", diff)
+    assert diff < threshold
